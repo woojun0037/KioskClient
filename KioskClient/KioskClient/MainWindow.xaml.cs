91755e7bf57820fd47989db9d4d0f1e3e5c44203
woojun0037 <67630480+woojun0037@users.noreply.github.com>
2026-04-14 00:42:28 +0900

    feat : connect WPF Clinet to API and display product list

---
+++ KioskClient/KioskClient/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using KioskClient.Models;
+
+namespace KioskClient
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoadProducts_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using var client = new HttpClient();
+
+                var products = await client.GetFromJsonAsync<List<Product>>("https://localhost:7183/api/products");
+
+                ProductsListBox.ItemsSource = products;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"상품을 불러오는 중 오류가 발생했습니다.\n{ex.Message}");
+            }
+        }
+    }
+}
---
+++ KioskClient/KioskClient/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KioskClient.Models
+{
+    class Product
+    {
+        public int Id { get; set;  }
+        public string Name { get; set; } = "";
+        public int Price { get; set; }
+    }
+}
