ee45f53277cdb542eec2f992565f1847e37204d6
woojun0037 <67630480+woojun0037@users.noreply.github.com>
2026-04-18 23:53:35 +0900

    feat : add update and delete features to WPF clinet

--- KioskClient/KioskClient/MainWindow.xaml.cs
+++ KioskClient/KioskClient/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http;
 using System.Net.Http.Json;
+using System.Printing;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -54,13 +55,13 @@ namespace KioskClient
                     return;
                 }
 
-                var request = new CreateaProductRequest
+                CreateaProductRequest request = new CreateaProductRequest
                 {
                     Name = NameTextBox.Text,
                     Price = price
                 };
 
-                var response = await _client.PostAsJsonAsync("api/products", request);
+                HttpResponseMessage response = await _client.PostAsJsonAsync("api/products", request);
 
                 if(!response.IsSuccessStatusCode)
                 {
@@ -79,17 +80,119 @@ namespace KioskClient
                 MessageBox.Show($"상품을 불러오는 중 오류가 발생했습니다.\n{ex.Message}");
             }
         }
+        
+        private async void UpdateProduct_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Product? selectedProduct = ProductsListBox.SelectedItem as Product;
+
+                if(selectedProduct == null)
+                {
+                    MessageBox.Show("수정 할 상품을 선택해 주세요");
+                    return;
+                }
+
+                if(string.IsNullOrWhiteSpace(NameTextBox.Text))
+                {
+                    MessageBox.Show(("상품명을 입력해주세요"));
+                    return;
+                }
+
+                if(!int.TryParse(PriceTextBox.Text, out int price))
+                {
+                    MessageBox.Show("가격은 숫자로 입력해주세요");
+                    return;
+                }
+
+                if(price <= 0 )
+                {
+                    MessageBox.Show("가격은 1 이상이어야 합니다.");
+                    return;
+                }
+
+                UpdateProductRequest request = new UpdateProductRequest
+                {
+                    Name = NameTextBox.Text,
+                    Price = price
+                };
+
+                var response = await _client.PutAsJsonAsync($"api/products/{selectedProduct.Id}", request);
+
+                if(!response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show($"상품 수정 실패 : {response.StatusCode}");
+                    return;
+                }
+                MessageBox.Show("상품이 수정되었습니다.");
+
+                await LoadProductsAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"상품 수정 중 오류 발생\n{ex.Message}");
+            }
+        }
+
+        private async void DeleteProduct_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Product? selectedProduct = ProductsListBox.SelectedItem as Product;
+                if(selectedProduct == null)
+                {
+                    MessageBox.Show("삭제 할 상품을 선택해 주세요");
+                    return;
+                }
+
+                MessageBoxResult result = MessageBox.Show("정말 삭제하시겠습니까?", "삭제 확인", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if(result != MessageBoxResult.Yes)
+                {
+                    return;
+                }
+
+                HttpResponseMessage response = await _client.DeleteAsync($"api/products/{selectedProduct.Id}");
+                if(!response.IsSuccessStatusCode)
+                {
+                    string errorMessage = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show($"상품 삭제 실패 : {response.StatusCode}\n{errorMessage}");
+                    return;
+                }
+
+                MessageBox.Show("상품이 삭제 되었습니다.");
+
+                NameTextBox.Clear();
+                PriceTextBox.Clear();
+
+                await LoadProductsAsync();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"상품 삭제 중 오류 발생\n{ex.Message}");
+            }
+        }
 
         private async Task LoadProductsAsync()
         {
             try
             {
-                var products = await _client.GetFromJsonAsync<List<Product>>("api/products");
+                List<Product>? products = await _client.GetFromJsonAsync<List<Product>>("api/products");
                 ProductsListBox.ItemsSource = products;
             }
             catch(Exception ex)
             {
                 MessageBox.Show($"상품을 불러오는 중 오류가 발생했습니다.\n{ex.Message}");
+            }
+        }
+
+        private void ProductsListBox_SelectionChanged(object sender, SelectionChangedEventArgs s)
+        {
+            Product? selectedProduct = ProductsListBox.SelectedItem as Product;
+
+            if(selectedProduct != null)
+            {
+                NameTextBox.Text = selectedProduct.Name;
+                PriceTextBox.Text = selectedProduct.Price.ToString();
             }
         }
     }
---
+++ KioskClient/KioskClient/Models/UpdateProductRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KioskClient.Models
+{
+    public class UpdateProductRequest
+    {
+        public string Name { get; set; } = "";
+        public int Price { get; set; }
+    }
+}
